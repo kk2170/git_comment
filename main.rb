334ab7e3a74411d91bd7d1fe2dfbb64d885d5053
kk2170 <chobi.kimura@gmail.com>
2016-11-19 01:06:43 +0900

    人毎にコミットログを追えるように修正。

--- main.rb
+++ main.rb
@@ -2,9 +2,18 @@ require 'pp'
 require 'grit'
 require 'natto'
 repo = Grit::Repo.new(ARGV[0])
+user_name = ARGV[1]
+
 words = []
 repo.log.each do |log|
   natto_mecab = Natto::MeCab.new
+
+  next if log.message.include?('Merge pull request ')
+
+  if user_name
+    next unless log.author.name == user_name
+  end
+
   natto_mecab.parse(log.message.force_encoding('UTF-8')) do |n|
     words << n.surface
   end
@@ -15,4 +24,4 @@ words.each do |word|
   words_and_count[word] += 1
 end
 
-pp words_and_count.sort_by{|_, v| -v }
+pp words_and_count.sort_by { |_, v| -v }
